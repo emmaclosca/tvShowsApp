26f414eb4f07d3771fb275923faf6e98de13cc5f
Emma Closca <x20514449@student.ncirl.ie>
2023-12-17 20:19:17 +0000

    resolved testing

--- test/system/shows_test.rb
+++ test/system/shows_test.rb
@@ -10,37 +10,41 @@ class ShowsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Shows"
   end
 
-  test "should create show" do
-    visit shows_url
-    click_on "New show"
+  # In test_should_create_show
+test "should create show" do
+  visit shows_url
+  click_on "New TV Show"
 
-    fill_in "Genre", with: @show.genre
-    fill_in "Language", with: @show.language
-    fill_in "Name", with: @show.name
-    fill_in "Rating", with: @show.rating
-    click_on "Create Show"
+  fill_in "Genre", with: @show.genre
+  fill_in "Language", with: @show.language
+  fill_in "Name", with: @show.name
+  fill_in "Rating", with: @show.rating
+  select users(:one).email, from: "User"  # Associate the show with a user
+  click_on "Create Show"
 
-    assert_text "Show was successfully created"
-    click_on "Back"
-  end
+  assert_text "Show was successfully created"
+  click_on "Return"
+end
 
-  test "should update Show" do
-    visit show_url(@show)
-    click_on "Edit this show", match: :first
+# In test_should_update_show
+test "should update show" do
+  visit show_url(@show)
+  click_on "Edit", match: :first
 
-    fill_in "Genre", with: @show.genre
-    fill_in "Language", with: @show.language
-    fill_in "Name", with: @show.name
-    fill_in "Rating", with: @show.rating
-    click_on "Update Show"
+  fill_in "Genre", with: @show.genre
+  fill_in "Language", with: @show.language
+  fill_in "Name", with: @show.name
+  fill_in "Rating", with: @show.rating
+  select users(:one).email, from: "User"  
+  click_on "Update Show"
 
-    assert_text "Show was successfully updated"
-    click_on "Back"
-  end
+  assert_text "Show was successfully updated"
+  click_on "Return"
+end
 
   test "should destroy Show" do
     visit show_url(@show)
-    click_on "Destroy this show", match: :first
+    click_on "Delete", match: :first
 
     assert_text "Show was successfully destroyed"
   end
